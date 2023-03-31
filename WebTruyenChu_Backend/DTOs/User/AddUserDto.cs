eec576f625e7f7a53865855fdf0c3bcd9d942185
pp311 <phucnhphc@gmail.com>
2023-03-31 21:32:34 +0700

    account Name -> optional

--- WebTruyenChu_Backend/DTOs/User/AddUserDto.cs
+++ WebTruyenChu_Backend/DTOs/User/AddUserDto.cs
@@ -4,25 +4,25 @@ namespace WebTruyenChu_Backend.DTOs;
 
 public class AddUserDto
 {
-    [Required]
+    // [Required]
     [MaxLength(256)]
-    public string? Name { get; set; }
+    public string? Name { get; set; } = "";
     [Required]
     [MaxLength(256)]
     public string? UserName { get; set; }
-        
+
     [Required]
     [MaxLength(256)]
     public string? Email { get; set; }
-        
+
     [Required]
     public string? Password { get; set; }
-        
+
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string? ConfirmPassword { get; set; }
-    
+
     [MaxLength(10)]
-    public string? Gender {get; set; }
-    
+    public string? Gender { get; set; }
+
     public DateTime? DateOfBirth { get; set; }
 }
